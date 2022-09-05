f8e676685dc35381cc27f01ee11b2b1d99d10ae2
spark61 <jan.fischer@zyonic.de>
2022-09-05 08:53:21 +0200

    add player

--- main.py
+++ main.py
@@ -5,20 +5,38 @@
 
 import pygame
 
+from player import Player
+
 pygame.init()
 
-screen = pygame.display.set_mode((1000, 1000))
+screen = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Jump and Run")
 
 running = True
 clock = pygame.time.Clock()
 
+player = Player()
+player_group = pygame.sprite.GroupSingle()
+player_group.add(player)
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                player.walk_x(-1)
+            elif event.key == pygame.K_d:
+                player.walk_x(1)
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a or event.key == pygame.K_d:
+                player.walk_x(0)
 
-    screen.fill((255,255,255))
+    screen.fill((255, 255, 255))
+
+    player_group.update()
+    player_group.draw(screen)
+
     pygame.display.update()
     clock.tick(60)
 
--- player.py
+++ player.py
@@ -1,15 +1,66 @@
-class Player:
-    def __init__(self, pygame):
-        self.texture = pygame.image.load("img/player.png").convert_alpha()
+import pygame
+from pygame import sprite
+
+
+class Player(sprite.Sprite):
+    def __init__(self):
+        sprite.Sprite.__init__(self)
+        self.walk_textures = []
+        self.run_textures = []
+        self.walk_texture_width = 16
+        self.run_texture_width = 20
+        self.texture_height = 16
+
+        walk_full_image = pygame.image.load("img/player/cat_0/cat_0_walk.png").convert_alpha()
+        run_full_image = pygame.image.load("img/player/cat_0/cat_0_run.png").convert_alpha()
+
+        for i in range(walk_full_image.get_width() // self.walk_texture_width):
+            self.walk_textures.append(pygame.transform.scale(
+                walk_full_image.subsurface(i * self.walk_texture_width, 0, self.walk_texture_width,
+                                           self.texture_height),
+                (self.walk_texture_width * 3, self.texture_height * 3)))
+
+        for i in range(walk_full_image.get_width() // self.run_texture_width):
+            self.run_textures.append(pygame.transform.scale(
+                run_full_image.subsurface(i * self.run_texture_width, 0, self.run_texture_width,
+                                          self.texture_height), (self.run_texture_width * 3, self.texture_height * 3)))
+
+        self.image = self.run_textures[0]
+        self.rect = self.image.get_rect()
+
         self.speed = 10
         self.posX = 200
         self.posY = 200
-        self.deltaX = 0
-        self.deltaY = 0
 
-    def move(self):
-        self.posX += self.deltaX
-        # self.posY += self.deltaY
+        self.jumping = 0
+        self.walk = 0
 
-    def print(self, screen):
-        screen.blit(self.texture, (self.posX, self.posY))
+        self.run_animation_index = 1
+
+    def walk_x(self, x):
+        self.walk = x
+
+    def jump(self):
+        self.jumping = 10
+
+    def update_image(self):
+        if self.walk != 0:
+            self.run_animation_index += 1
+
+            if self.run_animation_index >= 30:
+                self.run_animation_index = 0
+
+            self.image = self.run_textures[self.run_animation_index // 5]
+
+            if self.walk < 0:
+                self.image = pygame.transform.flip(self.image, True, False)
+
+        else:
+            self.image = self.walk_textures[0]
+
+    def update_position(self):
+        self.posX += self.walk
+
+    def update(self):
+        self.update_position()
+        self.update_image()
