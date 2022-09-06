d4a4536bbf6c72ef93f52c6c45e6c6d083fdfb14
spark61 <jan.fischer@zyonic.de>
2022-09-06 11:27:20 +0200

    add docu

--- platform.py
+++ platform.py
@@ -15,8 +15,9 @@ class Platform(sprite.Sprite):
         for path in os.listdir(dir_path):
             # check if current path is a file
             if os.path.isfile(os.path.join(dir_path, path)) and path.endswith(".png"):
-                self.tiles[path.replace(".png", "")] = pygame.transform.scale(pygame.image.load(dir_path + "/" + path),
-                                                                              (32, 32))
+                self.tiles[path.replace(".png", "")] = pygame.transform.scale(
+                    pygame.image.load(dir_path + "/" + path).convert_alpha(),
+                    (32, 32))
         if height == 1:
             self.image = self.tiles["floatingLeft"]
         else:
@@ -33,20 +34,19 @@ class Platform(sprite.Sprite):
 
         if height == 1:
             for x in range(width):
-                print(x)
-                if x == 0:
+                if x == 0:  # links
                     self.blocks.append([
                         start_x + texture_width * x,
                         start_y,
                         self.tiles["floatingLeft"]
                     ])
-                elif x == width - 1:
+                elif x == width - 1:  # rechts
                     self.blocks.append([
                         start_x + texture_width * x,
                         start_y,
                         self.tiles["floatingRight"]
                     ])
-                else:
+                else:  # mitte
                     self.blocks.append([
                         start_x + texture_width * x,
                         start_y,
@@ -56,63 +56,59 @@ class Platform(sprite.Sprite):
         else:
             for x in range(width):
                 for y in range(height):
-                    if x == 0 and y == 0:
+                    if x == 0 and y == 0:  # links oben
                         self.blocks.append([
                             start_x + texture_width * x,
                             start_y + texture_height * y,
                             self.tiles["wallTopLeft"]
                         ])
-                    elif x == width - 1 and y == 0:
+                    elif x == width - 1 and y == 0:  # rechts oben
                         self.blocks.append([
                             start_x + texture_width * x,
                             start_y + texture_height * y,
                             self.tiles["wallTopRight"]
                         ])
-                    elif x == 0 and y == height - 1:
-                        print(x, y)
+                    elif x == 0 and y == height - 1:  # links unten
                         self.blocks.append([
                             start_x + texture_width * x,
                             start_y + texture_height * y,
                             self.tiles["bottomCornerLeft"]
                         ])
-                    elif x == 0:
-                        print(x, y)
+                    elif x == 0:  # linke Seite
                         self.blocks.append([
                             start_x + texture_width * x,
                             start_y + texture_height * y,
                             self.tiles["wallLeft"]
                         ])
-                    elif x == width - 1 and y == height - 1:
-                        print(x, y)
+                    elif x == width - 1 and y == height - 1:  # rechts unten
                         self.blocks.append([
                             start_x + texture_width * x,
                             start_y + texture_height * y,
                             self.tiles["bottomCornerRight"]
                         ])
-                    elif x == width - 1:
-                        print(x, y)
+                    elif x == width - 1:  # rechte Seite
                         self.blocks.append([
                             start_x + texture_width * x,
                             start_y + texture_height * y,
                             self.tiles["wallRight"]
                         ])
-                    elif y == height - 1:
+                    elif y == height - 1:  # untere Seite
                         self.blocks.append([
                             start_x + texture_width * x,
                             start_y + texture_height * y,
                             self.tiles["bottomMiddle"]
                         ])
-                    elif y != 0:
-                        self.blocks.append([
-                            start_x + texture_width * x,
-                            start_y + texture_height * y,
-                            self.tiles["wallMiddle"]
-                        ])
-                    else:
+                    elif y == 0:  # obere Seite
                         self.blocks.append([
                             start_x + texture_width * x,
                             start_y + texture_height * y,
                             self.tiles["wallTopMiddle"]
+                        ])
+                    else:  # mitte
+                        self.blocks.append([
+                            start_x + texture_width * x,
+                            start_y + texture_height * y,
+                            self.tiles["wallMiddle"]
                         ])
 
     def update(self, screen):
